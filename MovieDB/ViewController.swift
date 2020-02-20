ea24a52c65d4692be344232e26ea69ed64184f88
Armen Nagapetyan <armennagapetyan1996@gmail.com>
2020-02-20 11:29:02 +0200

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Armen Nagapetyan on 20.02.2020.
+//  Copyright © 2020 Armen Nagapetyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
